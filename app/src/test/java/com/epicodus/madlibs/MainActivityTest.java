a1fbc99df8384ab1f641d2da5e8c5ee40a440318
Anna Lawley and tyler suderman <alawley1@gmail.com>
2016-04-19 10:44:02 -0700

    add two unit tests

---
+++ app/src/test/java/com/epicodus/madlibs/MainActivityTest.java
@@ -0,0 +1,45 @@
+package com.epicodus.madlibs;
+
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by Guest on 4/19/16.
+ */
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+public class MainActivityTest {
+    private static final String TAG = "MyActivity";
+    private MainActivity activity;
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView appNameTextView = (TextView) activity.findViewById(R.id.appTitleText);
+        assertTrue("Mad Libs".equals(appNameTextView.getText().toString()));
+    }
+
+    @Test
+    public void secondActivityStarted() {
+        activity.findViewById(R.id.submitWordsButton).performClick();
+        Intent expectedIntent = new Intent(activity, SubmitActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+}
