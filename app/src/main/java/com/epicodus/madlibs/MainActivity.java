9de1cb34e4939d3b34793a863bad54e573d74755
Anna Lawley and tyler suderman <alawley1@gmail.com>
2016-04-19 08:50:31 -0700

    gather user inputted words

--- app/src/main/java/com/epicodus/madlibs/MainActivity.java
+++ app/src/main/java/com/epicodus/madlibs/MainActivity.java
@@ -5,19 +5,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mSubmitWordsButton;
+    private EditText mAdjectiveText;
+    private EditText mVerbText;
+    private EditText mNounText;
+    private EditText mAdverbText;
+    private EditText mProperNounText;
+    private ArrayList<String> userInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAdjectiveText = (EditText) findViewById(R.id.adjectiveText);
+        mVerbText = (EditText) findViewById(R.id.verbText);
+        mNounText = (EditText) findViewById(R.id.nounText);
+        mAdverbText = (EditText) findViewById(R.id.adverbText);
+        mProperNounText = (EditText) findViewById(R.id.properNounText);
         mSubmitWordsButton = (Button) findViewById(R.id.submitWordsButton);
         mSubmitWordsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                userInput.add(mAdjectiveText.getText().toString());
+                userInput.add(mVerbText.getText().toString());
+                userInput.add(mNounText.getText().toString());
+                userInput.add(mAdverbText.getText().toString());
+                userInput.add(mProperNounText.getText().toString());
                 Intent intent = new Intent(MainActivity.this, SubmitActivity.class);
                 startActivity(intent);
             }
